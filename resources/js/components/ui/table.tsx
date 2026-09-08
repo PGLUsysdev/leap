2e357085750a3835ebd359cfd495a11a4413984d
codesalotl <jaspertoribio10@gmail.com>
2026-09-08 09:15:02 +0800

    fix table header/footer sticky

--- resources/js/components/ui/table.tsx
+++ resources/js/components/ui/table.tsx
@@ -5,7 +5,8 @@ function Table({ className, ...props }: React.ComponentProps<"table">) {
   return (
     <div
       data-slot="table-container"
-      className="relative w-full overflow-x-auto"
+      // className="relative w-full overflow-x-auto"
+      className="relative w-full"
     >
       <table
         data-slot="table"
